63d379a697cb45bb240207704a561dac92d0e6cc
Jason Maderski <maderski@gmail.com>
2025-04-22 22:15:01 -0400

    remove default template code

--- shared/src/androidMain/kotlin/com/maderskitech/kmptruerandom/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.maderskitech.kmptruerandom
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
--- shared/src/commonMain/kotlin/com/maderskitech/kmptruerandom/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.maderskitech.kmptruerandom
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
